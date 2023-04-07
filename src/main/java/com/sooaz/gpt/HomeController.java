cd91fc1229b368be56da229fca5be1561b6be957
gyuray <hodduck0121@gmail.com>
2023-04-07 16:09:00 +0900

    eungyu test

--- src/main/java/com/sooaz/gpt/HomeController.java
+++ src/main/java/com/sooaz/gpt/HomeController.java
@@ -24,6 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		//test eungyu
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
