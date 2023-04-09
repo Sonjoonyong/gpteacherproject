e5304c96f18cbc39585f1a42c16fbd02446ab3fb
gyuray-dev <choieungyu.dev@gmail.com>
2023-04-09 22:54:13 +0900

    Config and test MyBatis Dependency
    - DDL 추가
    - MyBatis-spring 의존성 추가
    - MyBatis 설정
      - SqlSessionFactoryBean 빈 설정
      - MapperScanner 빈 설정
      - DataSourceTransactionManager 빈 설정
    - 테스트용 작성
      - 테스트 위해 User 도메인 및 관련 클래스 추가
      - DB 및 MyBatis 테스트 코드 추가

---
+++ src/main/java/com/sooaz/gpt/domain/user/User.java
@@ -0,0 +1,17 @@
+package com.sooaz.gpt.domain.user;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+
+    private Long id;
+    private String userLoginId;
+    private String userPassword;
+    private String userNickname;
+    private String userEmail;
+    private Date userBirthday;
+
+}
