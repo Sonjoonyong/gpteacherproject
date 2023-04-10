2548f4b4da1bc073ae1244b6fabbd043f6cbc56e
gyuray-dev <choieungyu.dev@gmail.com>
2023-04-10 22:59:13 +0900

    Add Learning, Sentence and their dummy repository

--- src/main/java/com/sooaz/gpt/domain/mypage/sentence/Sentence.java
+++ src/main/java/com/sooaz/gpt/domain/mypage/sentence/Sentence.java
@@ -1,4 +1,20 @@
 package com.sooaz.gpt.domain.mypage.sentence;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Sentence {
+    private Long id;
+    private Long learningId;
+    private Long flashcardId;
+    private Long voidFileId;
+    private String sentenceQuestion;
+    private String sentenceAnswer;
+    private String sentenceCorrected;
+    private Character sentenceLike;
+    private String sentenceAccuracy;
+    private Date sentenceNextRepetition;
+    private Integer sentenceRepetitionStep;
 }
