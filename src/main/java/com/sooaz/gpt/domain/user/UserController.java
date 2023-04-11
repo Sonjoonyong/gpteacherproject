9d86a496f1c38e74cbb20d14964d4f0ad1978fed
KangSuA <nol.dev.sua@gmail.com>
2023-04-11 18:15:12 +0900

    Add Learning DB

--- src/main/java/com/sooaz/gpt/domain/user/UserController.java
+++ src/main/java/com/sooaz/gpt/domain/user/UserController.java
@@ -1,4 +1,12 @@
 package com.sooaz.gpt.domain.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+    @GetMapping("/users/login")
+    public String getLoginForm() {
+        return "user/login";
+    }
 }
