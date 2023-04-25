a17cd0ee200ba7f0755f4683cd7f0816e1e8e6cc
gyuray <choieungyu.dev@gmail.com>
2023-04-25 17:17:16 +0900

    Change Email validation from session to Redis
    - 기존 session을 통해 진행하던 이메일 인증을 Redis로 변경함
    - Redis 의존성 추가 및 관련 상수는 RedisConst.java에 정의함
    - 이메일 인증 번호 유효기간은 3분, 이메일 인증 후 회원가입 유효기간은 10분으로 설정
    - 그 외 비밀번호 찾기 관련 버그 수정

--- src/main/java/com/sooaz/gpt/domain/mypage/UserEditPwDto.java
+++ src/main/java/com/sooaz/gpt/domain/mypage/UserEditPwDto.java
@@ -11,7 +11,7 @@ public class UserEditPwDto {
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String userPassword;
 
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{6,12}$", message = "비밀번호는 6~12 자리의 영문자, 숫자 및 특수문자($@$!%*#?&)를 포함해 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,12}$", message = "비밀번호는 6~12 자리의 영문자, 숫자 및 특수문자(@$!%*#?&)를 포함해 입력해주세요.")
     private String userNewPassword;
 
     private String userNewPasswordCheck;
--- src/main/java/com/sooaz/gpt/domain/user/UserService.java
+++ src/main/java/com/sooaz/gpt/domain/user/UserService.java
@@ -1,10 +1,14 @@
 package com.sooaz.gpt.domain.user;
 
+import com.sooaz.gpt.global.constant.RedisConst;
+import com.sooaz.gpt.global.email.Gmail;
 import com.sooaz.gpt.global.security.PasswordHasher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,10 +22,12 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordHasher passwordHasher;
+    private final JedisPool jedisPool;
+    private final Gmail gmail;
 
-    public boolean isCorrectPassword(User user, String inputPassord) {
+    public boolean isCorrectPassword(User user, String inputPassword) {
         String passwordSalt = user.getUserPasswordSalt();
-        String hashedPassword = passwordHasher.hash(inputPassord, passwordSalt);
+        String hashedPassword = passwordHasher.hash(inputPassword, passwordSalt);
         return user.getUserPassword().equals(hashedPassword);
     }
 
@@ -64,7 +70,6 @@ public class UserService {
     }
 
     @Transactional(readOnly = true)
-
     public Optional<User> findById(Long userId) {
         return userRepository.findById(userId);
     }
@@ -96,19 +101,81 @@ public class UserService {
         userRepository.update(userUpdateDto);
     }
 
+    @Transactional(readOnly = true)
     public boolean isDuplicateLoginId(String loginId) {
         return userRepository.findByLoginId(loginId).isPresent();
     }
 
+    @Transactional(readOnly = true)
     public boolean isDuplicateNickname(String nickname) {
         return userRepository.findByNickname(nickname).isPresent();
     }
 
+    @Transactional(readOnly = true)
     public boolean isDuplicateEmail(String email) {
         return userRepository.findByEmail(email).isPresent();
     }
 
     public void delete(Long userId) {
         userRepository.delete(userId);
+    }
+
+    public String getEmailCode(String userEmail) {
+        String emailCode = UUID.randomUUID().toString()
+                .replaceAll("-", "").substring(0, 5);
+        log.info("발급된 emailCode = {}", emailCode);
+
+        gmail.sendEmail(
+                userEmail,
+                "GPTeacher 회원가입 인증코드입니다.",
+                "아래 코드를 인증 창에 입력 후 회원가입을 진행하세요. \n\n" + emailCode
+        );
+
+        try (Jedis jedis = jedisPool.getResource()) {
+            // 이메일 인증 코드 유효 기간 3분
+            jedis.setex(
+                    RedisConst.USER_EMAIL_CODE + userEmail,
+                    RedisConst.USER_EMAIL_CODE_EXP,
+                    emailCode
+            );
+        } catch (Exception e) {
+            log.error("Redis error: ", e);
+        }
+
+        return emailCode;
+    }
+
+    public boolean isValidEmailCode(String userEmail, String emailCode) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            String foundEmailCode = jedis.get(RedisConst.USER_EMAIL_CODE + userEmail);
+            return emailCode.equals(foundEmailCode);
+        } catch (Exception e) {
+            log.error("Redis error: ", e);
+        }
+
+        return false;
+    }
+
+    public void setEmailValidated(String userEmail) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            // 이메일 인증 유효 기간 10분
+            jedis.setex(
+                    RedisConst.USER_EMAIL_VALIDATED + userEmail,
+                    RedisConst.USER_EMAIL_VALIDATED_EXP,
+                    "true");
+        } catch (Exception e) {
+            log.error("Redis error: ", e);
+        }
+    }
+
+    public boolean isEmailValidated(String userEmail) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            String isValidated = jedis.get(RedisConst.USER_EMAIL_VALIDATED + userEmail);
+            return isValidated.equals("true");
+        } catch (Exception e) {
+            log.error("Redis error: ", e);
+        }
+
+        return false;
     }
 }
