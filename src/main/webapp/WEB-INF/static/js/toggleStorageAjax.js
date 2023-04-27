5fdd403c2cffc42b54e55ca098bb91b7f8f70a25
KangSuA <nol.dev.sua@gmail.com>
2023-04-27 14:43:09 +0900

    Add pronunciation to flashcard

--- src/main/webapp/WEB-INF/static/js/toggleStorageAjax.js
+++ src/main/webapp/WEB-INF/static/js/toggleStorageAjax.js
@@ -1,16 +1,16 @@
 function toggleStorageAjax(btn) {
     let request = new XMLHttpRequest();
     let sentenceId = btn.closest('.sentence').id.replace('sentence_', '');
-    let like = btn.querySelector('.storage');
+    let storage = btn.querySelector('.storage');
 
     request.onload = () => {
         let result = request.response;
         if (result === '0') {
-            like.classList.toggle('bi-archive', true);
-            like.classList.toggle('bi-archive-fill', false);
+            storage.classList.toggle('bi-archive', true);
+            storage.classList.toggle('bi-archive-fill', false);
         } else if (result === '1') {
-            like.classList.toggle('bi-archive', false);
-            like.classList.toggle('bi-archive-fill', true);
+            storage.classList.toggle('bi-archive', false);
+            storage.classList.toggle('bi-archive-fill', true);
         } else {
             alert("존재하지 않는 문장입니다.");
         }
